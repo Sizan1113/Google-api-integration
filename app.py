57da1c53d6f397e33b0e83b2d9ce1352c2004ffe
Sizan1113 <sizanraut11@gmail.com>
2026-02-12 18:47:40 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from google import genai
+ 
+client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
+ 
+def gemini_llm(content):
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents = content
+    )
+    return response.text
+ 
+ 
+ 
